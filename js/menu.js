30fae6bae49674bef4539a1ce7fc4abafba5a1f5
Tu Nombre <tucorreo@ejemplo.com>
2026-02-28 10:46:21 -0500

    Push cambios

--- js/menu.js
+++ js/menu.js
@@ -137,3 +137,9 @@
             }
         });
     });
+
+    fetch("/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
